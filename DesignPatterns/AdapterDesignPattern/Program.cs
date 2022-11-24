bbc8c1556efbb49f8c2f3b81613bf13e4ef73162
sc185488 <117810743+sc185488@users.noreply.github.com>
2022-11-24 18:49:41 +0530

    Add files via upload

---
+++ DesignPatterns/AdapterDesignPattern/Program.cs
@@ -0,0 +1,20 @@
+﻿using DesignPatternsNew.AdapterDesignPattern.Adapter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatternsNew
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            EmployeeManager employeeManager = new EmployeeManager();
+            Console.WriteLine(employeeManager.GetAllEmployeesInXml());
+            IEmployee iEmployee = new EmployeeAdapter(new EmployeeManager());
+            Console.WriteLine(iEmployee.GetAllEmployees()); 
+        }
+    }
+}
